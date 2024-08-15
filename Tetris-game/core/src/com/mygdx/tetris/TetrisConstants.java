94ec8af8b4a011a066049086b45710bbfd35d767
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2024-08-15 19:04:55 +0300

    Add files via upload

---
+++ Tetris-game/core/src/com/mygdx/tetris/TetrisConstants.java
@@ -0,0 +1,22 @@
+package com.mygdx.tetris;
+
+public class TetrisConstants {
+    public static final int
+            SCREEN_WIDTH = 300,
+            SCREEN_HEIGHT = 600,
+            SCREEN_CENTER_X = SCREEN_WIDTH / 2,
+            SCREEN_CENTER_Y = SCREEN_HEIGHT / 2,
+            GAME_HEIGHT = 450,
+            GAME_WIDTH = 300,
+            SPAWN_HEIGHT = 480,
+            GAME_LOWEST_X = 0,
+            GAME_LOWEST_Y = 0;
+    public static final int CELL_SIZE = 30;
+    public static final int LIMIT_LINE_LENGTH = 60;
+    public static final double
+            TIME_BETWEEN_MOVES_MILLISECONDS = 200,
+            TIME_BETWEEN_DROPS_MILLISECONDS = 400;
+    static final int
+            SCORE_X = 10,
+            SCORE_Y = SCREEN_HEIGHT - 10;
+}
---
+++ Tetris-game/core/src/com/mygdx/tetris/tetronimoes/RightZigzagTetronimo.java
@@ -0,0 +1,83 @@
+package com.mygdx.tetris.tetronimoes;
+
+import com.badlogic.gdx.utils.Array;
+import com.mygdx.tetris.Cell;
+import com.mygdx.tetris.TetrisConstants;
+
+public class RightZigzagTetronimo extends Tetronimo{
+    public RightZigzagTetronimo(Cell centerCell, Array<Cell> boardCells) {
+        super(centerCell, boardCells);
+    }
+
+    @Override
+    void setRotationStateUp(Array<Cell> boardCells) {
+        Array<Cell> newCells = new Array<>();
+        newCells.add(tetronimoCells.get(0));
+        newCells.add(new Cell(tetronimoCells.get(0).x, tetronimoCells.get(0).y + TetrisConstants.CELL_SIZE));
+        newCells.add(new Cell(tetronimoCells.get(0).x - TetrisConstants.CELL_SIZE, tetronimoCells.get(0).y));
+        newCells.add(new Cell(tetronimoCells.get(0).x - TetrisConstants.CELL_SIZE, tetronimoCells.get(0).y - TetrisConstants.CELL_SIZE));
+
+        if (isPlaceable(newCells, boardCells)) {
+            isRotationStateUp = true;
+            isRotationStateDown = false;
+            isRotationStateLeft = false;
+            isRotationStateRight = false;
+
+            tetronimoCells = newCells;
+        }
+    }
+
+    @Override
+    void setRotationStateRight(Array<Cell> boardCells) {
+        Array<Cell> newCells = new Array<>();
+        newCells.add(tetronimoCells.get(0));
+        newCells.add(new Cell(tetronimoCells.get(0).x - TetrisConstants.CELL_SIZE, tetronimoCells.get(0).y));
+        newCells.add(new Cell(tetronimoCells.get(0).x, tetronimoCells.get(0).y - TetrisConstants.CELL_SIZE));
+        newCells.add(new Cell(tetronimoCells.get(0).x + TetrisConstants.CELL_SIZE, tetronimoCells.get(0).y - TetrisConstants.CELL_SIZE));
+
+        if (isPlaceable(newCells, boardCells)) {
+            isRotationStateRight = true;
+            isRotationStateUp = false;
+            isRotationStateDown = false;
+            isRotationStateLeft = false;
+
+            tetronimoCells = newCells;
+        }
+    }
+
+    @Override
+    void setRotationStateDown(Array<Cell> boardCells) {
+        Array<Cell> newCells = new Array<>();
+        newCells.add(tetronimoCells.get(0));
+        newCells.add(new Cell(tetronimoCells.get(0).x, tetronimoCells.get(0).y + TetrisConstants.CELL_SIZE));
+        newCells.add(new Cell(tetronimoCells.get(0).x - TetrisConstants.CELL_SIZE, tetronimoCells.get(0).y));
+        newCells.add(new Cell(tetronimoCells.get(0).x - TetrisConstants.CELL_SIZE, tetronimoCells.get(0).y - TetrisConstants.CELL_SIZE));
+
+        if (isPlaceable(newCells, boardCells)) {
+            isRotationStateDown = true;
+            isRotationStateUp = false;
+            isRotationStateRight = false;
+            isRotationStateLeft = false;
+
+            tetronimoCells = newCells;
+        }
+    }
+
+    @Override
+    void setRotationStateLeft(Array<Cell> boardCells) {
+        Array<Cell> newCells = new Array<>();
+        newCells.add(tetronimoCells.get(0));
+        newCells.add(new Cell(tetronimoCells.get(0).x - TetrisConstants.CELL_SIZE, tetronimoCells.get(0).y));
+        newCells.add(new Cell(tetronimoCells.get(0).x, tetronimoCells.get(0).y - TetrisConstants.CELL_SIZE));
+        newCells.add(new Cell(tetronimoCells.get(0).x + TetrisConstants.CELL_SIZE, tetronimoCells.get(0).y - TetrisConstants.CELL_SIZE));
+
+        if (isPlaceable(newCells, boardCells)) {
+            isRotationStateLeft = true;
+            isRotationStateUp = false;
+            isRotationStateRight = false;
+            isRotationStateDown = false;
+
+            tetronimoCells = newCells;
+        }
+    }
+}
